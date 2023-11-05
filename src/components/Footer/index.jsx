e771802295335071de724d3480f0b5bf58512c35
Cesar Almeida <cesaralmeida89@gmail.com>
2023-11-05 19:43:13 +0100

    add Footer styles

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,23 +1,26 @@
 import { ArrowRightIcon } from '@heroicons/react/24/solid'
 import githubLogoSvg from '../../assets/github-mark.svg'
 import linkedinLogoPng from '../../assets/LI-In-Bug.png'
+import cesarLogoJpg from '../../assets/tiny-cesar-2020-1080_74KB.jpg'
 
 function Footer () {
   return (
-    <footer>
-      <div>
-        <img src='' alt='the developer' />
-        <h4>César Almeida</h4>
-        <p>Frontend Developer</p>
-      </div>
-      <section>
+    <footer className='flex flex-col md:flex-row md:items-start m-auto lg:m-0 md:pl-5 gap-5 md:gap-16 py-10 md:p-10'>
+      <article className='flex items-center md:items-start gap-5'>
+        <img className='w-24 rounded-full' src={cesarLogoJpg} alt='the developer' />
         <div>
-          <h5>Get in youch</h5>
-          <ArrowRightIcon className='w-6 h-6 text-blue-500' />
+          <h4 className='font-semibold text-2xl'>César Almeida</h4>
+          <p className='font-semibold text-gray-500'>Frontend Developer</p>
         </div>
-        <ul>
-          <li><a href=''><img src={githubLogoSvg} alt='' /></a></li>
-          <li><a href=''><img src={linkedinLogoPng} alt='' /></a></li>
+      </article>
+      <section className='flex flex-col gap-5'>
+        <div className='flex gap-2 items-center'>
+          <h5 className='text-2xl font-bold'>Get in youch</h5>
+          <ArrowRightIcon className='w-8 h-8 text-blue-500' />
+        </div>
+        <ul className='flex gap-4 items-center'>
+          <li><a className='flex gap-1 items-center' href='https://www.linkedin.com/in/cesaralmeidadev/' target='_blank' ><img className='w-10' src={linkedinLogoPng} alt='' />Ir a LinkedIn</a></li>
+          <li><a className='flex gap-1 items-center' href='https://github.com/cesarforall' target='_blank' ><img className='w-10 h-10' src={githubLogoSvg} alt='' />Ir a GitHub</a></li>
         </ul>
       </section>
     </footer>
