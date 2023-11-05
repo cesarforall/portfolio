ebb96e731fae90b845dd2c2191f5e216a739048f
Cesar Almeida <cesaralmeida89@gmail.com>
2023-11-05 18:05:26 +0100

    add page Portfolio

---
+++ src/pages/Portfolio/index.jsx
@@ -0,0 +1,18 @@
+import { PROJECTS } from '../../data'
+import ProjectCard from '../../components/ProjectCard'
+
+function Portfolio () {
+  return (
+    <section className='flex flex-col gap-4'>
+      <h2 className='text-center text-4xl font-semibold'>Portfolio</h2>
+      <p className='text-center text-xs'>Proyectos destacados</p>
+      <ul className='grid grid-cols-autoFit200 p-4 gap-3'>
+        {
+            PROJECTS.map((project, index) => <ProjectCard key={index} project={project} />)
+          }
+      </ul>
+    </section>
+  )
+}
+
+export default Portfolio
