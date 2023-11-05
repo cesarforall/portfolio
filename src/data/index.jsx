f24975057c307b8b51e4559e60dd73ce28ca69bd
Cesar Almeida <cesaralmeida89@gmail.com>
2023-11-05 17:57:14 +0100

    add data

---
+++ src/data/index.jsx
@@ -0,0 +1,57 @@
+export const PROJECTS = [
+  {
+    name: 'Nombre del proyecto',
+    description: 'Descripción del proyecto',
+    technologies: [
+      'React',
+      'JavaScript',
+      'HTML',
+      'CSS'
+    ],
+    link: '/'
+  },
+  {
+    name: 'Nombre del proyecto',
+    description: 'Descripción del proyecto',
+    technologies: [
+      'React',
+      'JavaScript',
+      'HTML',
+      'CSS'
+    ],
+    link: '/'
+  },
+  {
+    name: 'Nombre del proyecto',
+    description: 'Descripción del proyecto',
+    technologies: [
+      'React',
+      'JavaScript',
+      'HTML',
+      'CSS'
+    ],
+    link: '/'
+  },
+  {
+    name: 'Nombre del proyecto',
+    description: 'Descripción del proyecto',
+    technologies: [
+      'React',
+      'JavaScript',
+      'HTML',
+      'CSS'
+    ],
+    link: '/'
+  },
+  {
+    name: 'Nombre del proyecto',
+    description: 'Descripción del proyecto',
+    technologies: [
+      'React',
+      'JavaScript',
+      'HTML',
+      'CSS'
+    ],
+    link: '/'
+  }
+]
