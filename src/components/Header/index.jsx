285d14bd13bec6ce1c0eb5c1edfbf27a996988b7
Cesar Almeida <cesaralmeida89@gmail.com>
2023-11-20 17:51:52 +0100

    fit header size

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,10 +3,14 @@ import { CodeBracketIcon } from '@heroicons/react/24/solid'
 function Header () {
   return (
     <header>
-      <nav className='p-4'>
+      <nav className='p-6'>
         <div className='flex gap-2 justify-center md:justify-normal'>
-          <CodeBracketIcon className='h-8 w-8 text-blue-600' />
-          <h1 className=' text-3xl font-medium'>Programador</h1>
+          <CodeBracketIcon className='h-6 w-6 text-blue-600' />
+          <div className='text-center'>
+            <h1 className=' text-xl font-medium'>César Almeida</h1>
+            <p className='text-xs font-semibold text-gray-500'>Frontend Developer</p>
+          </div>
+          <CodeBracketIcon className='h-6 w-6 text-blue-600' />
         </div>
       </nav>
     </header>
