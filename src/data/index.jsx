7a683e04654123c1da9648a3db12f3c62caad443
Cesar Almeida <cesaralmeida89@gmail.com>
2023-11-21 16:33:37 +0100

    update links data

--- src/data/index.jsx
+++ src/data/index.jsx
@@ -3,19 +3,6 @@ import onlineShopPNG from '../assets/online-shop.png'
 
 export const PROJECTS = [
   {
-    name: 'TODO Machine',
-    description: 'Web app to manage tasks',
-    imgLink: todoMachinePNG,
-    technologies: [
-      'React',
-      'JavaScript',
-      'HTML',
-      'CSS'
-    ],
-    appLink: 'https://cesarforall.github.io/todo-machine/',
-    repoLink: 'https://github.com/cesarforall/todo-machine'
-  },
-  {
     name: 'Online Shop',
     description: 'Online shop demo with filters and checkout',
     imgLink: onlineShopPNG,
@@ -25,7 +12,20 @@ export const PROJECTS = [
       'HTML',
       'CSS'
     ],
-    appLink: 'https://cesarforall.github.io/todo-machine/',
+    appLink: 'https://glowing-souffle-dc41d6.netlify.app',
+    repoLink: 'https://github.com/cesarforall/online-shop'
+  },
+  {
+    name: 'TODO Machine',
+    description: 'Web app to manage tasks',
+    imgLink: todoMachinePNG,
+    technologies: [
+      'React',
+      'JavaScript',
+      'HTML',
+      'CSS'
+    ],
+    appLink: 'https://cesarforall.github.io/todo-machine',
     repoLink: 'https://github.com/cesarforall/todo-machine'
   }
 ]
